2cc0c39da25c792a4c51e9973895024f12880686
ntphu <ntphu240701@gmail.com>
2023-09-27 14:57:45 +0700

    Phu_DAT

--- src/components/Lib/Slider.js
+++ src/components/Lib/Slider.js
@@ -4,29 +4,28 @@ function Slider() {
     return (
         <div>
             <Box sx={{ width: 300 }}>
-      <Slider
-        aria-label="Temperature"
-        defaultValue={30}
-        getAriaValueText={valuetext}
-        valueLabelDisplay="on"
-        step={1}
-        marks
-        min={0}
-        max={100}
-      />
-      <Slider
-        valueLabelDisplay="on"
-        defaultValue={30}
-        
-        step={1}
-        marks
-        min={0}
-        max={100}
-        
-      />
-    </Box>
+              <Slider
+                aria-label="Temperature"
+                defaultValue={30}
+                getAriaValueText={valuetext}
+                valueLabelDisplay="on"
+                
+                min={0}
+                max={100}
+              />
+              <Slider
+                valueLabelDisplay="on"
+                defaultValue={30}
+                
+                step={1}
+                marks
+                min={0}
+                max={100}
+                
+              />
+            </Box>
         </div>
-      
+        
     );
 }
 
