e91edce321ee952ee2c77db041c41ffc0ef949ba
ntphu <ntphu240701@gmail.com>
2023-10-17 17:29:18 +0700

    PHU_DAT

--- src/components/ErrorReport/ErrorReport.js
+++ src/components/ErrorReport/ErrorReport.js
@@ -171,30 +171,32 @@ export default function ErrorReport() {
   return (
     <div className="DAT_Content">
       <div className="DAT_Content-Header">
-        <div className="DAT_Content-Header-Dashboard">
-          <div className="DAT_Content-Header-Dashboard-Heading">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="28"
-              height="28"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="feather feather-filter"
-              style={{ paddingRight: "10px" }}
-            >
-              <polyline points="17 1 21 5 17 9"></polyline>
-              <path d="M3 11V9a4 4 0 0 1 4-4h14"></path>
-              <polyline points="7 23 3 19 7 15"></polyline>
-              <path d="M21 13v2a4 4 0 0 1-4 4H3"></path>
-            </svg>
-            Báo Cáo Lỗi
-          </div>
-          <div className="DAT_Content-Header-Dashboard-SubHead">
-            Example dashboard overview and content summary
+        <div className="DAT_Content-Header-Main">
+          <div className="DAT_Content-Header-Main-Dashboard">
+            <div className="DAT_Content-Header-Main-Dashboard-Heading">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="28"
+                height="28"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="feather feather-filter"
+                style={{ paddingRight: "10px" }}
+              >
+                <polyline points="17 1 21 5 17 9"></polyline>
+                <path d="M3 11V9a4 4 0 0 1 4-4h14"></path>
+                <polyline points="7 23 3 19 7 15"></polyline>
+                <path d="M21 13v2a4 4 0 0 1-4 4H3"></path>
+              </svg>
+              Báo Cáo Lỗi
+            </div>
+            <div className="DAT_Content-Header-Main-Dashboard-SubHead">
+              Example dashboard overview and content summary
+            </div>
           </div>
         </div>
       </div>
