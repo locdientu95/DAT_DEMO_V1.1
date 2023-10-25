f14b3b7744ef173abf364a850f4673392b7fda62
HuynhHuuu <HUU.H9155@sinhvien.hoasen.edu.vn>
2023-10-25 09:56:55 +0700

    HUU_DAT

--- src/components/Lib/LineChartLib.js
+++ src/components/Lib/LineChartLib.js
@@ -1,31 +1,40 @@
-import React, { useRef } from "react";
-import Chart from "chart.js/auto";
+import React, { useEffect, useRef, useState } from "react";
+import Chart, { scales } from "chart.js/auto";
 import { Line } from "react-chartjs-2";
 import zoomPlugin from "chartjs-plugin-zoom";
 
 Chart.register(zoomPlugin)
 export default function LineChartLib(props) {
+  
+  const [datetime, setDateTime] = useState()
+  const [newLabel, setNewLabel] = useState([])
+  const [lastData, setLastData] = useState([])
   const config = {
     type: 'line',
+
     
       plugins: {
+        
         zoom: {
           limits: {
             y: {min: 0, max: 100},
-            y2: {min: -5, max: 5}
+            y2: {min: -5, max: 5},
           },
           zoom: {
             wheel: {
               enabled: true,
+
             },
             pinch: {
               enabled: true
             },
             mode: 'xy',
+            
+            
           }
         }
-      }
-    
+      },
+      
   }
   const LineRef = useRef();
   const handleResetZoom = () => {
@@ -45,11 +54,40 @@ export default function LineChartLib(props) {
       },
     ]
   }
+  const newData ={ 
+    labels:newLabel,
+    datasets:[
+      {
+        label: "Test dataset",
+        data:lastData,
+        fill:true,
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "#4BC0C0",
+      }
+    ],
+  }
 
+  useEffect(() => { 
+    const interval = setInterval(() => {
+     
+      const date = new Date()
+      const today = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()
+      setDateTime(today)
+      setNewLabel(pre=>[...pre,today])
+      setLastData(pre=>[...pre,10])
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+
+  useEffect(()=>{
+    console.log(newLabel,lastData)
+  },[newLabel,lastData])
+ 
   return (
     <div>
       <div>
-        <Line data={data} options={config} ref={LineRef} />
+        <Line data={newData} options={config} ref={LineRef} />
       </div>
       <button onClick={handleResetZoom}>Reset</button>
     </div>
