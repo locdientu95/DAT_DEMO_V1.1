59673f098d7ac0c527dfd3a0fb85fde49ea7340c
ntphu <ntphu240701@gmail.com>
2023-11-22 14:58:08 +0700

    PHU_DAT

--- src/components/ExportReport/UploadFile.js
+++ src/components/ExportReport/UploadFile.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./ExportReport.scss";
 import axios from "axios";
+import { IoCloudUpload } from "react-icons/io5";
 
 export default function UploadFile() {
   const [filename, setFilename] = useState(null);
@@ -24,15 +25,46 @@ export default function UploadFile() {
 
   return (
     <div className="DAT_UploadFile">
-      <form className="DAT_UploadFile-Form" onSubmit={onSubmit}>
-        <input type="file" id="file" onChange={onInputChange} />
-        <input type="submit" value="Submit" />
+      <form
+        className="DAT_UploadFile-Form"
+        onClick={() => document.querySelector(".inputfile").click()}
+        // onSubmit={onSubmit}
+      >
+        <input
+          type="file"
+          id="file"
+          className="inputfile"
+          hidden
+          // onChange={onInputChange}
+        />
+        <div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              marginBottom: "10px",
+            }}
+          >
+            <IoCloudUpload color="#0061f2" size={50} />
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              fontSize: "18px",
+            }}
+          >
+            Kéo và thả file vào đây
+          </div>
+        </div>
+        {/* <input type="submit" value="Submit" /> */}
       </form>
 
-      <div>
-        {/* <div className="DAT_UploadFile-Select-Label">Chọn form</div> */}
-        <select className="DAT_UploadFile-Select">
-          <option>Chọn form</option>
+      <div className="DAT_UploadFile-Select">
+        <div>Chọn form</div>
+        <select>
           <option>a</option>
           <option>b</option>
           <option>c</option>
@@ -40,7 +72,7 @@ export default function UploadFile() {
       </div>
 
       <div className="DAT_UploadFile-Button">
-        <button disabled>Tải file</button>
+        <button>Tải file</button>
       </div>
     </div>
   );
