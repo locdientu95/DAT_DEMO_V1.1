2189d7ab819782777fe2c377cd0cd8db1b762d45
HuynhHuuu <HUU.H9155@sinhvien.hoasen.edu.vn>
2023-12-14 17:32:43 +0700

    HUU_DAT

--- src/components/ExportReport/Report.js
+++ src/components/ExportReport/Report.js
@@ -1,13 +1,46 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./ExportReport.scss";
 import { FaFileExcel } from "react-icons/fa";
 
 export default function ListForm() {
   const [form, setForm] = useState("Month");
+  const timetype = useRef()
+  const timestart = useRef()
+  const timeend = useRef()
+  const device = useRef()
+  const date = useRef()
+  const startDate = useRef()
+  const endDate = useRef()
   const handleChange = (e) => {
     setForm(e.target.value);
     console.log(e.target.value);
   };
+
+  const handleExportDay=()=>{
+    const obj = {
+      ExportType: form,
+      Device: device.current.value,
+      Date: date.current.value,
+      Timetype: timetype.current.value,
+      TimeStart: timestart.current.value,
+      TimeEnd: timeend.current.value 
+    }
+    console.log(obj)
+  }
+
+  const handleExportMonth=()=>{
+    const obj = {
+      ExportType: form,
+      Device: device.current.value,
+      DateStart: startDate.current.value,
+      DateEnd: endDate.current.value 
+    }
+    if (obj.DateStart > obj.DateEnd){
+      alert("Nhập sai ngày tháng năm gòi")
+    }else
+    console.log(obj)
+  }
+
   return (
     <div className="DAT_Report">
       <div className="DAT_Report-Left">
@@ -36,25 +69,25 @@ export default function ListForm() {
                     <div className="DAT_Report-Right-Content">
                       <div className="DAT_Report-Right-Content-Select">
                         <span>Thiết bị</span>
-                        <select>
-                          <option>Nghiền đĩa đế 1 </option>
-                          <option>Nghiền đĩa đế 2 </option>
-                          <option>Nghiền đĩa đế 3</option>
-                          <option>Nghiền đĩa đế 4</option>
-                          <option>Thủy lực đế 3</option>
-                          <option>Thủy lực đế 4</option>
-                          <option>Thủy lực đế 6</option>
-                          <option>Thủy lực đế 7</option>
-                          <option>Thủy lực màu 1</option>
-                          <option>Thủy lực màu 2</option>
+                        <select ref={device}>
+                          <option value="MC1">Nghiền đĩa đế 1 </option>
+                          <option value="MC2">Nghiền đĩa đế 2 </option>
+                          <option value="MC3">Nghiền đĩa đế 3</option>
+                          <option value="MC4">Nghiền đĩa đế 4</option>
+                          <option value="MC5">Thủy lực đế 3</option>
+                          <option value="MC6">Thủy lực đế 4</option>
+                          <option value="MC7">Thủy lực đế 6</option>
+                          <option value="MC8">Thủy lực đế 7</option>
+                          <option value="MC9">Thủy lực màu 1</option>
+                          <option value="MC10">Thủy lực màu 2</option>
                         </select>
                       </div>
 
                       <div className="DAT_Report-Right-Content-Date">
                         <span>Từ ngày</span>
-                        <input type="date" />
+                        <input type="date" ref={startDate}/>
                         <span>Đến ngày</span>
-                        <input type="date" />
+                        <input type="date" ref={endDate}/>
                       </div>
 
                       <div className="DAT_Report-Right-Content-Button">
@@ -62,7 +95,7 @@ export default function ListForm() {
                           Xuất file tại đây -{">"}
                         </span>
                         <div className="DAT_Report-Right-Content-Button-Icon">
-                          <FaFileExcel size={24} style={{ color: "green" }} />
+                          <FaFileExcel size={24} style={{ color: "green" }} onClick={handleExportMonth} />
                         </div>
                       </div>
                     </div>
@@ -84,17 +117,17 @@ export default function ListForm() {
                         <span style={{ width: "80px", textAlign: "left" }}>
                           Thiết bị
                         </span>
-                        <select>
-                          <option>Nghiền đĩa đế 1 </option>
-                          <option>Nghiền đĩa đế 2 </option>
-                          <option>Nghiền đĩa đế 3</option>
-                          <option>Nghiền đĩa đế 4</option>
-                          <option>Thủy lực đế 3</option>
-                          <option>Thủy lực đế 4</option>
-                          <option>Thủy lực đế 6</option>
-                          <option>Thủy lực đế 7</option>
-                          <option>Thủy lực màu 1</option>
-                          <option>Thủy lực màu 2</option>
+                        <select ref={device}>
+                          <option value="MC1">Nghiền đĩa đế 1 </option>
+                          <option value="MC2">Nghiền đĩa đế 2 </option>
+                          <option value="MC3">Nghiền đĩa đế 3</option>
+                          <option value="MC4">Nghiền đĩa đế 4</option>
+                          <option value="MC5">Thủy lực đế 3</option>
+                          <option value="MC6">Thủy lực đế 4</option>
+                          <option value="MC7">Thủy lực đế 6</option>
+                          <option value="MC8">Thủy lực đế 7</option>
+                          <option value="MC9">Thủy lực màu 1</option>
+                          <option value="MC10">Thủy lực màu 2</option>
                         </select>
                       </div>
 
@@ -102,25 +135,25 @@ export default function ListForm() {
                         <span style={{ width: "80px", textAlign: "left" }}>
                           Chọn ngày
                         </span>
-                        <input type="date" />
+                        <input type="date" ref={date}/>
                       </div>
 
                       <div className="DAT_Report-Right-Content-Select">
                         <span style={{ width: "80px", textAlign: "left" }}>
                           Khung giờ
                         </span>
-                        <select>
-                          <option>Tùy chỉnh</option>
-                          <option>Giờ cao điểm</option>
-                          <option>Giờ thấp điểm</option>
-                          <option>Giờ bình thường</option>
+                        <select ref={timetype}>
+                          <option value="Setting">Tùy chỉnh</option>
+                          <option value="High">Giờ cao điểm</option>
+                          <option value="Low">Giờ thấp điểm</option>
+                          <option value="Normal">Giờ bình thường</option>
                         </select>
                       </div>
 
                       <div className="DAT_Report-Right-Content-Date2">
                         <span>Từ giờ</span>
                         <div className="DAT_Report-Right-Content-Select">
-                          <select>
+                          <select ref={timestart}>
                             <option>0h</option>
                             <option>0h30</option>
                             <option>...</option>
@@ -128,7 +161,7 @@ export default function ListForm() {
                         </div>
                         <span>Đến giờ</span>
                         <div className="DAT_Report-Right-Content-Select">
-                          <select>
+                          <select ref={timeend}>
                             <option>0h</option>
                             <option>0h30</option>
                             <option>...</option>
@@ -141,7 +174,7 @@ export default function ListForm() {
                           Xuất file tại đây -{">"}
                         </span>
                         <div className="DAT_Report-Right-Content-Button-Icon">
-                          <FaFileExcel size={24} style={{ color: "green" }} />
+                          <FaFileExcel size={24} style={{ color: "green" }} onClick={handleExportDay} />
                         </div>
                       </div>
                     </div>
