cdeea4d2478edb204d0de8f926bad66677f4a8d1
ntphu <ntphu240701@gmail.com>
2023-12-15 08:20:34 +0700

    PHU_DAT

--- src/components/ExportReport/Report.js
+++ src/components/ExportReport/Report.js
@@ -1,13 +1,78 @@
 import React, { useState } from "react";
 import "./ExportReport.scss";
 import { FaFileExcel } from "react-icons/fa";
+import { Box } from "../Lib/FunctionElement";
 
 export default function ListForm() {
+  const time = [
+    "00:00",
+    "00:30",
+    "01:00",
+    "01:30",
+    "02:00",
+    "02:30",
+    "03:00",
+    "03:30",
+    "04:00",
+    "04:30",
+    "05:00",
+    "05:30",
+    "06:00",
+    "06:30",
+    "07:00",
+    "07:30",
+    "08:00",
+    "08:30",
+    "09:00",
+    "09:30",
+    "10:00",
+    "10:30",
+    "11:00",
+    "11:30",
+    "12:00",
+    "12:30",
+    "13:00",
+    "13:30",
+    "14:00",
+    "14:30",
+    "15:00",
+    "15:30",
+    "16:00",
+    "16:30",
+    "17:00",
+    "17:30",
+    "18:00",
+    "18:30",
+    "19:00",
+    "19:30",
+    "20:00",
+    "20:30",
+    "21:00",
+    "21:30",
+    "22:00",
+    "22:30",
+    "23:00",
+    "23:30",
+  ];
+
+  const device = [
+    "Nghiền đĩa đế 1 ",
+    "Nghiền đĩa đế 2 ",
+    "Nghiền đĩa đế 3",
+    "Nghiền đĩa đế 4",
+    "Thủy lực đế 3",
+    "Thủy lực đế 4",
+    "Thủy lực đế 6",
+    "Thủy lực đế 7",
+    "Thủy lực màu 1",
+    "Thủy lực màu 2",
+  ];
+
   const [form, setForm] = useState("Month");
   const handleChange = (e) => {
     setForm(e.target.value);
-    console.log(e.target.value);
   };
+
   return (
     <div className="DAT_Report">
       <div className="DAT_Report-Left">
@@ -36,18 +101,7 @@ export default function ListForm() {
                     <div className="DAT_Report-Right-Content">
                       <div className="DAT_Report-Right-Content-Select">
                         <span>Thiết bị</span>
-                        <select>
-                          <option>Nghiền đĩa đế 1 </option>
-                          <option>Nghiền đĩa đế 2 </option>
-                          <option>Nghiền đĩa đế 3</option>
-                          <option>Nghiền đĩa đế 4</option>
-                          <option>Thủy lực đế 3</option>
-                          <option>Thủy lực đế 4</option>
-                          <option>Thủy lực đế 6</option>
-                          <option>Thủy lực đế 7</option>
-                          <option>Thủy lực màu 1</option>
-                          <option>Thủy lực màu 2</option>
-                        </select>
+                        {Box(device)}
                       </div>
 
                       <div className="DAT_Report-Right-Content-Date">
@@ -84,18 +138,7 @@ export default function ListForm() {
                         <span style={{ width: "80px", textAlign: "left" }}>
                           Thiết bị
                         </span>
-                        <select>
-                          <option>Nghiền đĩa đế 1 </option>
-                          <option>Nghiền đĩa đế 2 </option>
-                          <option>Nghiền đĩa đế 3</option>
-                          <option>Nghiền đĩa đế 4</option>
-                          <option>Thủy lực đế 3</option>
-                          <option>Thủy lực đế 4</option>
-                          <option>Thủy lực đế 6</option>
-                          <option>Thủy lực đế 7</option>
-                          <option>Thủy lực màu 1</option>
-                          <option>Thủy lực màu 2</option>
-                        </select>
+                        {Box(device)}
                       </div>
 
                       <div className="DAT_Report-Right-Content-Date1">
@@ -120,19 +163,11 @@ export default function ListForm() {
                       <div className="DAT_Report-Right-Content-Date2">
                         <span>Từ giờ</span>
                         <div className="DAT_Report-Right-Content-Select">
-                          <select>
-                            <option>0h</option>
-                            <option>0h30</option>
-                            <option>...</option>
-                          </select>
+                          {Box(time)}
                         </div>
                         <span>Đến giờ</span>
                         <div className="DAT_Report-Right-Content-Select">
-                          <select>
-                            <option>0h</option>
-                            <option>0h30</option>
-                            <option>...</option>
-                          </select>
+                          {Box(time)}
                         </div>
                       </div>
 
